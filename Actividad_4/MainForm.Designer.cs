a09b4009663429bb499485a21d6c8318012f7dfe
Hector <hectorgarx2@gmail.com>
2023-09-12 22:06:25 -0600

    first commit

---
+++ Actividad_4/MainForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: chomp
+ * Date: 12/09/2023
+ * Time: 02:36 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Actividad_4
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.progressBar2 = new System.Windows.Forms.ProgressBar();
+			this.button3 = new System.Windows.Forms.Button();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.progressBar3 = new System.Windows.Forms.ProgressBar();
+			this.button4 = new System.Windows.Forms.Button();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.progressBar4 = new System.Windows.Forms.ProgressBar();
+			this.button5 = new System.Windows.Forms.Button();
+			this.checkBox5 = new System.Windows.Forms.CheckBox();
+			this.progressBar5 = new System.Windows.Forms.ProgressBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblProcess1 = new System.Windows.Forms.Label();
+			this.lblProcess2 = new System.Windows.Forms.Label();
+			this.lblProcess3 = new System.Windows.Forms.Label();
+			this.lblProcess4 = new System.Windows.Forms.Label();
+			this.lblProcess5 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.BackColor = System.Drawing.Color.Red;
+			this.progressBar1.Location = new System.Drawing.Point(217, 80);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(312, 39);
+			this.progressBar1.TabIndex = 38;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(24, 80);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(92, 39);
+			this.checkBox1.TabIndex = 43;
+			this.checkBox1.Text = "Proceso1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Enabled = false;
+			this.button1.Location = new System.Drawing.Point(113, 80);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(89, 39);
+			this.button1.TabIndex = 44;
+			this.button1.Text = "Empezar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Enabled = false;
+			this.button2.Location = new System.Drawing.Point(113, 138);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(89, 39);
+			this.button2.TabIndex = 47;
+			this.button2.Text = "Empezar";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Location = new System.Drawing.Point(24, 138);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(92, 39);
+			this.checkBox2.TabIndex = 46;
+			this.checkBox2.Text = "Proceso2";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// progressBar2
+			// 
+			this.progressBar2.BackColor = System.Drawing.Color.Red;
+			this.progressBar2.Location = new System.Drawing.Point(217, 138);
+			this.progressBar2.Name = "progressBar2";
+			this.progressBar2.Size = new System.Drawing.Size(312, 39);
+			this.progressBar2.TabIndex = 45;
+			// 
+			// button3
+			// 
+			this.button3.Enabled = false;
+			this.button3.Location = new System.Drawing.Point(113, 200);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(89, 39);
+			this.button3.TabIndex = 50;
+			this.button3.Text = "Empezar";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.Location = new System.Drawing.Point(24, 200);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(92, 39);
+			this.checkBox3.TabIndex = 49;
+			this.checkBox3.Text = "Proceso3";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// progressBar3
+			// 
+			this.progressBar3.BackColor = System.Drawing.Color.Red;
+			this.progressBar3.Location = new System.Drawing.Point(217, 200);
+			this.progressBar3.Name = "progressBar3";
+			this.progressBar3.Size = new System.Drawing.Size(312, 39);
+			this.progressBar3.TabIndex = 48;
+			// 
+			// button4
+			// 
+			this.button4.Enabled = false;
+			this.button4.Location = new System.Drawing.Point(113, 262);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(89, 39);
+			this.button4.TabIndex = 53;
+			this.button4.Text = "Empezar";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.Location = new System.Drawing.Point(24, 262);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(92, 39);
+			this.checkBox4.TabIndex = 52;
+			this.checkBox4.Text = "Proceso4";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// progressBar4
+			// 
+			this.progressBar4.BackColor = System.Drawing.Color.Red;
+			this.progressBar4.Location = new System.Drawing.Point(217, 262);
+			this.progressBar4.Name = "progressBar4";
+			this.progressBar4.Size = new System.Drawing.Size(312, 39);
+			this.progressBar4.TabIndex = 51;
+			// 
+			// button5
+			// 
+			this.button5.Enabled = false;
+			this.button5.Location = new System.Drawing.Point(113, 317);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(89, 39);
+			this.button5.TabIndex = 56;
+			this.button5.Text = "Empezar";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// checkBox5
+			// 
+			this.checkBox5.Location = new System.Drawing.Point(24, 317);
+			this.checkBox5.Name = "checkBox5";
+			this.checkBox5.Size = new System.Drawing.Size(92, 39);
+			this.checkBox5.TabIndex = 55;
+			this.checkBox5.Text = "Proceso5";
+			this.checkBox5.UseVisualStyleBackColor = true;
+			// 
+			// progressBar5
+			// 
+			this.progressBar5.BackColor = System.Drawing.Color.Red;
+			this.progressBar5.Location = new System.Drawing.Point(217, 317);
+			this.progressBar5.Name = "progressBar5";
+			this.progressBar5.Size = new System.Drawing.Size(312, 39);
+			this.progressBar5.TabIndex = 54;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(149, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(408, 41);
+			this.label1.TabIndex = 57;
+			this.label1.Text = "5 estados de los procesos";
+			// 
+			// lblProcess1
+			// 
+			this.lblProcess1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblProcess1.Location = new System.Drawing.Point(545, 80);
+			this.lblProcess1.Name = "lblProcess1";
+			this.lblProcess1.Size = new System.Drawing.Size(124, 35);
+			this.lblProcess1.TabIndex = 58;
+			// 
+			// lblProcess2
+			// 
+			this.lblProcess2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblProcess2.Location = new System.Drawing.Point(545, 138);
+			this.lblProcess2.Name = "lblProcess2";
+			this.lblProcess2.Size = new System.Drawing.Size(124, 35);
+			this.lblProcess2.TabIndex = 59;
+			// 
+			// lblProcess3
+			// 
+			this.lblProcess3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblProcess3.Location = new System.Drawing.Point(545, 204);
+			this.lblProcess3.Name = "lblProcess3";
+			this.lblProcess3.Size = new System.Drawing.Size(124, 35);
+			this.lblProcess3.TabIndex = 60;
+			// 
+			// lblProcess4
+			// 
+			this.lblProcess4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblProcess4.Location = new System.Drawing.Point(545, 262);
+			this.lblProcess4.Name = "lblProcess4";
+			this.lblProcess4.Size = new System.Drawing.Size(124, 35);
+			this.lblProcess4.TabIndex = 61;
+			// 
+			// lblProcess5
+			// 
+			this.lblProcess5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblProcess5.Location = new System.Drawing.Point(545, 321);
+			this.lblProcess5.Name = "lblProcess5";
+			this.lblProcess5.Size = new System.Drawing.Size(124, 35);
+			this.lblProcess5.TabIndex = 62;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(700, 399);
+			this.Controls.Add(this.lblProcess5);
+			this.Controls.Add(this.lblProcess4);
+			this.Controls.Add(this.lblProcess3);
+			this.Controls.Add(this.lblProcess2);
+			this.Controls.Add(this.lblProcess1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.checkBox5);
+			this.Controls.Add(this.progressBar5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.checkBox4);
+			this.Controls.Add(this.progressBar4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.progressBar3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.progressBar2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.progressBar1);
+			this.Name = "MainForm";
+			this.Text = "Hector Emmanuel Garcia Garcia Actividad_4";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label lblProcess5;
+		private System.Windows.Forms.Label lblProcess4;
+		private System.Windows.Forms.Label lblProcess3;
+		private System.Windows.Forms.Label lblProcess2;
+		private System.Windows.Forms.Label lblProcess1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBox5;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.ProgressBar progressBar2;
+		private System.Windows.Forms.ProgressBar progressBar5;
+		private System.Windows.Forms.ProgressBar progressBar4;
+		private System.Windows.Forms.ProgressBar progressBar3;
+	}
+}
---
+++ Actividad_4/MainForm.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Hector Emmanuel Garcia Garcia
+ * Date: 12/09/2023
+ * Time: 02:36 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Actividad_4
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			while (progressBar1.Value != 100) {
+				progressBar1.Value++;
+			}			
+		}
+	}
+}
